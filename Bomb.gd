436465d6948bc687fe4fc6e88554a40cdaf936e0
Ambarish S <imrealambarish@gmail.com>
2026-06-11 20:45:02 +0000

    added bomb explosion and timer f

--- Bomb.gd
+++ Bomb.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+const EXPLOSION_SCENE = preload("res://Explosion.tscn")
+
 func _ready():
 	$Timer.timeout.connect(_on_timer_timeout)
 	$Timer.start()
 
 func _on_timer_timeout():
+	var explosion = EXPLOSION_SCENE.instantiate()
+	explosion.position = position
+
+	get_tree().current_scene.add_child(explosion)
+
 	queue_free()
