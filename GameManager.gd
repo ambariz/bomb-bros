3307d24b04608e214d7bf6d0c5f2503e79adadbf
Ambarish S <imrealambarish@gmail.com>
2026-06-14 12:56:26 +0000

    game completed

---
+++ GameManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var p1_score = 0
+var p2_score = 0
+
+func reset_scores():
+	p1_score = 0
+	p2_score = 0
--- Main.gd
+++ Main.gd
@@ -1,34 +1,63 @@
 extends Node2D
 
+const POINTS_TO_WIN = 5
+
 var game_over = false
+var round_over = false
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	update_score_label()
 
 func _process(_delta):
-	if game_over and Input.is_key_pressed(KEY_R):
+
+	if round_over and !game_over and Input.is_key_pressed(KEY_C):
 		get_tree().paused = false
 		get_tree().reload_current_scene()
 
-func player_died(_player_name):
+	if game_over and Input.is_key_pressed(KEY_R):
+		GameManager.reset_scores()
+		get_tree().paused = false
+		get_tree().reload_current_scene()
 
-	if game_over:
+func player_died(player_name):
+
+	if game_over or round_over:
 		return
 
-	await get_tree().process_frame
+	round_over = true
 
-	var p1_alive = get_node_or_null("Player1") != null
-	var p2_alive = get_node_or_null("Player2") != null
+	if player_name == "Player1":
+		GameManager.p2_score += 1
 
+	elif player_name == "Player2":
+		GameManager.p1_score += 1
+
+	update_score_label()
+
+	if GameManager.p1_score >= POINTS_TO_WIN:
+		end_game("PLAYER 1 WINS THE GAME!")
+
+	elif GameManager.p2_score >= POINTS_TO_WIN:
+		end_game("PLAYER 2 WINS THE GAME!")
+
+	else:
+		$WinnerLabel.text = "P1: %d   P2: %d\nPress C To Continue" % [
+			GameManager.p1_score,
+			GameManager.p2_score
+		]
+
+		get_tree().paused = true
+
+func update_score_label():
+	$ScoreLabel.text = "Player One: %d   Player Two: %d" % [
+		GameManager.p1_score,
+		GameManager.p2_score
+	]
+
+func end_game(message):
 	game_over = true
 
-	if !p1_alive and !p2_alive:
-		$WinnerLabel.text = "DRAW!\nPress R"
-
-	elif !p1_alive:
-		$WinnerLabel.text = "PLAYER 2 WINS!\nPress R"
-
-	elif !p2_alive:
-		$WinnerLabel.text = "PLAYER 1 WINS!\nPress R"
+	$WinnerLabel.text = message + "\nPress R To Restart"
 
 	get_tree().paused = true
